29fb611ba897fb8e12f21536204ae9e2eb8bcfcc
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2025-09-20 10:53:12 +0600

    bookings header done

---
+++ src/app/(admin)/bookings/page.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import BookingsHeader from "./_components/BookingsHeader";
+
+const page = () => {
+  return (
+    <div>
+      <BookingsHeader />
+    </div>
+  );
+};
+
+export default page;

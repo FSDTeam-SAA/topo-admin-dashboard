05792218c5af9ca645bebe48c495dcba520b8e4c
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2025-09-20 11:25:08 +0600

    search bookings seciton done

--- src/app/(admin)/bookings/page.tsx
+++ src/app/(admin)/bookings/page.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import BookingsHeader from "./_components/BookingsHeader";
+import SearchBookings from "./_components/search-bookings";
 
 const page = () => {
   return (
     <div>
       <BookingsHeader />
+      <SearchBookings />
     </div>
   );
 };

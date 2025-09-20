3d3e52904074a84afa55b2449da06179a5c2e2b5
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2025-09-20 12:03:54 +0600

    bookings table done

--- src/app/(admin)/bookings/page.tsx
+++ src/app/(admin)/bookings/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import BookingsHeader from "./_components/BookingsHeader";
 import SearchBookings from "./_components/search-bookings";
+import BookingsTable from "./_components/bookings-table";
 
 const page = () => {
   return (
     <div>
       <BookingsHeader />
       <SearchBookings />
+      <BookingsTable />
     </div>
   );
 };

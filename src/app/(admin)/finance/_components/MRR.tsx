12d6757d5556d4cf2da2cae914283d96f2476ed2
rifat-mahmudul <rifatmahmudul.dev@gmail.com>
2025-12-20 10:58:35 +0600

    mrr trend done

--- src/app/(admin)/finance/_components/MRR.tsx
+++ src/app/(admin)/finance/_components/MRR.tsx
@@ -2,6 +2,22 @@
 import React from "react";
 import MrrStates from "./mrr-states";
 import { useQuery } from "@tanstack/react-query";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Card, CardContent } from "@/components/ui/card";
+import {
+  type ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import { CartesianGrid, Line, LineChart, XAxis } from "recharts";
 
 interface CommonTypes {
   _id: string;
@@ -13,6 +29,11 @@ interface CommonTypes {
   status: string;
 }
 
+interface MrrTrend {
+  month: string;
+  mrr: string;
+}
+
 export interface MrrData {
   totalMRR: string;
   totalNewSignUps: string;
@@ -20,7 +41,15 @@ export interface MrrData {
   activeSubscribers: CommonTypes[];
   newSignUps: CommonTypes[];
   churnedUsers: CommonTypes[];
+  mrrTrend: MrrTrend[];
 }
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "var(--chart-1)",
+  },
+} satisfies ChartConfig;
 
 const MRR = ({ token }: { token: string }) => {
   const { data: mrrData } = useQuery({
@@ -41,10 +70,80 @@ const MRR = ({ token }: { token: string }) => {
     },
   });
 
+  const chartData = mrrData?.mrrTrend || [];
+
   return (
     <div className="space-y-8">
       <div>
         <MrrStates mrrData={mrrData} />
+
+        <div className="bg-white shadow-[0px_4px_10px_0px_#0000001A] p-5 rounded-lg space-y-8">
+          <div className="flex items-center justify-between">
+            <h1 className="text-xl ">MRR Trend</h1>
+
+            <Select>
+              <SelectTrigger className="w-[180px]">
+                <SelectValue placeholder="Select" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectGroup>
+                  <SelectItem value="apple">Weekly</SelectItem>
+                  <SelectItem value="banana">Monthly</SelectItem>
+                  <SelectItem value="blueberry">Yearly</SelectItem>
+                </SelectGroup>
+              </SelectContent>
+            </Select>
+          </div>
+
+          <div>
+            <Card className="h-[400px]">
+              <CardContent className="h-full">
+                <ChartContainer config={chartConfig} className="h-full w-full">
+                  <LineChart
+                    accessibilityLayer
+                    data={chartData}
+                    margin={{
+                      left: 30,
+                      right: 12,
+                    }}
+                  >
+                    <CartesianGrid vertical={false} />
+                    <XAxis
+                      dataKey="month"
+                      tickLine={false}
+                      axisLine={false}
+                      tickMargin={8}
+                      textAnchor="end"
+                      height={50}
+                      angle={-45}
+                      interval={0}
+                      tickFormatter={(value) => {
+                        const [year, month] = value.split("-");
+                        const date = new Date(`${year}-${month}-01`);
+                        const monthName = date.toLocaleDateString("en-US", {
+                          month: "short",
+                        });
+                        const yearShort = year.slice(2);
+                        return `${monthName} '${yearShort}`;
+                      }}
+                    />
+                    <ChartTooltip
+                      cursor={false}
+                      content={<ChartTooltipContent hideLabel />}
+                    />
+                    <Line
+                      dataKey="mrr"
+                      type="natural"
+                      stroke="#891d33"
+                      strokeWidth={2}
+                      dot={true}
+                    />
+                  </LineChart>
+                </ChartContainer>
+              </CardContent>
+            </Card>
+          </div>
+        </div>
       </div>
     </div>
   );
